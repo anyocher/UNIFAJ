2f9fbbae3d981eed5336cc575c686b608d028784
Ana Ochner <140626175+anyocher@users.noreply.github.com>
2026-05-29 19:31:46 -0300

    Add successor and predecessor calculation program

---
+++ sucessor.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int numero, antecessor, sucessor;
+
+    cout << "DIGITE UM NÚMERO PARA SABER SEU ANTECESSOR E SUCESSOR: ";
+    cin >> numero;
+
+    antecessor = numero - 1;
+    sucessor = numero + 1;
+
+    cout << "ANTECESSOR " << numero << " DE: " << antecessor << endl;
+    cout << "SUCESSOR " << numero << " DE: " << sucessor << endl;
+
+    return 0;
+}
